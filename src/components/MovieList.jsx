12e6fc1c378fda7654c9e997f91aa160ff098760
Navin Kumar <111299104+Navinksinhaa@users.noreply.github.com>
2023-05-01 16:31:59 +0530

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,17 @@
+import {Row,Col} from 'react-bootstrap';
+
 export const MovieList = (props) => {
     const FavouriteComponent = props.favouriteComponent;
     return (
-        <div className="container-fluid">
-        
-            <div className="col-auto">
-            {props.movies.map((movie,index) => (
-            <div className="image-container d-flex justify-content-start m3">
+        <div className="container-fluid row">
+            <Row>
+                <Col>
+            <div className="row" style={{border: "2px solid white"}}>
+                    {props.movies.map((movie,index) => (
+            <div className="image-container m3" style={{width:'305px',margin:'22px'}}>
                 <img src={movie.Poster} alt='movie' />
             
-            <div className="col-sm">
+            <div className="col" style={{ marginLeft:'60px', width:'200px'}}>
                 <button
                 onClick = {() =>props.handleFavouritesClick(movie)}
                 className="btn btn-danger btn-block"
@@ -19,7 +22,10 @@ export const MovieList = (props) => {
             </div>
            ))}  
 
-            </div>
+            </div>       
+                </Col>
+            </Row>
+            
         
               
         
